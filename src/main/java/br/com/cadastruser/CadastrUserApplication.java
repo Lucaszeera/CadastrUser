f3cac1d50e141273b59a5712ae57c11d7b448d1b
= <lucas.hbrd@gmail.com>
2023-03-10 17:44:12 -0300

    inicial do CadastrUser rodando com sucesso

---
+++ src/main/java/br/com/cadastruser/CadastrUserApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cadastruser/CadastrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cadastruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
