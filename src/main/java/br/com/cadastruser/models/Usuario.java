5acb49cfa82dc0b5d96ae7b116210f7c0a7a3d23
= <lucas.hbrd@gmail.com>
2023-04-06 03:26:14 -0300

    commitando tudo para daqui começar o git flow

--- src/main/java/br/com/cadastruser/models/Usuario.java
+++ src/main/java/br/com/cadastruser/models/Usuario.java
@@ -1,78 +1,41 @@
 package br.com.cadastruser.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 @Entity
+@Table(name = "CU_USUARIO")
 public class Usuario {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
     @NotBlank
-    String nome;
-    @NotNull @Min(0)
-    int idade;
+    private String nome;
+    @Min(0)
+    private int idade;
+    @NotBlank @JsonProperty(access = Access.WRITE_ONLY)
+    private String cpf;
+    private String numero;
     @NotBlank
-    String cpf;
-    String numero;
-    @NotBlank
-    String email;
+    private String email;
+    @NotNull @JsonProperty(access = Access.WRITE_ONLY)
+    private String senha;
     
-
     public Usuario() {
     }
-
-    public Usuario(Long id, String nome, int idade, String cpf, String numero, String email) {
-        this.id = id;
-        this.nome = nome;
-        this.idade = idade;
-        this.cpf = cpf;
-        this.numero = numero;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public int getIdade() {
-        return idade;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    public String getCpf() {
-        return cpf;
-    }
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-    public String getNumero() {
-        return numero;
-    }
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
     
 }
  
